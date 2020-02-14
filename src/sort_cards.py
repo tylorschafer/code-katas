186480ae8b513446a967860438977ce389b9ccdc
Tylor Schafer <tylorschafer@gmail.com>
2020-02-14 09:17:26 -0700

    Remove print statements from sort_cards

--- src/sort_cards.py
+++ src/sort_cards.py
@@ -11,14 +11,10 @@ def sort_cards(cards):
 
     letters = sorted([card for card in cards if card.isalpha()], reverse=True)
     numbers = sorted([card for card in cards if card.isdigit()])
-    print(letters)
-    print(numbers)
 
     sorted_cards = numbers + letters
-    print(sorted_cards)
 
     for _ in range(a_count):
         sorted_cards.insert(0, 'A')
 
-    print(sorted_cards)
     return sorted_cards
